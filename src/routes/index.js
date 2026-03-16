b5c6159ec184297ea3857320046143033833df86
Jonathan Ndakola <j.ndakola1@gmail.com>
2026-03-16 15:39:23 -0400

    nice

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAnalyticsPage, getWorkflowsPage, getAgentsPage, getHomePage, getSettingsPage } from "../controllers/pages.js";
+import { getAnalyticsPage, getWorkflowsPage, getWorkflowDetailPage, getAgentsPage, getHomePage, getSettingsPage } from "../controllers/pages.js";
 import { chat, invokeLambda } from "../controllers/chat.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/", getHomePage);
 router.get("/agents", getAgentsPage);
 router.get("/workflows", getWorkflowsPage);
+router.get("/workflows/:id", getWorkflowDetailPage);
 router.get("/analytics", getAnalyticsPage);
 router.get("/settings", getSettingsPage);
 
